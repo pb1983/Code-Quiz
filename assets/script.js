5ac257657b45ef632a89a31f168226ef5fbff4be
pb1983 <phil.bryer@gmail.com>
2023-07-30 16:29:48 -0400

    Created 'highscores.html'. Added css styling to buttons. Set query selectors for each answer.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,17 @@
+
+let h1El = document.querySelector("#question");
+
 let quizDiv = document.querySelector("#quiz");
+
 
 let time = 90;
 
-let questionButton1 = document.querySelector("answer1");
+let questionButton1 = document.querySelector("#answer1");
+let questionButton2 = document.querySelector("#answer2");
+let questionButton3 = document.querySelector("#answer3");
+let questionButton4 = document.querySelector("#answer4");
 
+let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 let timerID = setInterval(function(){
 
@@ -25,27 +33,22 @@ renderQuestion();
 function renderQuestion() {
 
 
-
-questions[currentQuestion].question
+h1El.textContent = questions[currentQuestion].question
 questionButton1.textContent = questions[currentQuestion].answers[0]
-questions[currentQuestion].answers[1]
-questions[currentQuestion].answers[2]
-questions[currentQuestion].answers[3]
+questionButton2.textContent = questions[currentQuestion].answers[1]
+questionButton3.textContent = questions[currentQuestion].answers[2]
+questionButton4.textContent = questions[currentQuestion].answers[3]
 
-questions[currentQuestion].answers[0]
-questions[currentQuestion].answers[1]
-questions[currentQuestion].answers[2]
-questions[currentQuestion].answers[3]
-console.log("correct answer:" + questions[currentQuestion].correctAnswer);
 
 }
 
 
 quizDiv.addEventListener("click", function(event){
     if(event.target.matches("button")){
-        console.log("click!")
-
+        console.log("click!");
         currentQuestion++
-        RenderQuestion();
+        renderQuestion();
     }
 })
+
+//put JSON.stringify in the button that saves the score and player initials
