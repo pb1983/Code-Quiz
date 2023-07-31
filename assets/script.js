f81037eec62f8a41f7490931b9633cc6d7ffb276
pb1983 <phil.bryer@gmail.com>
2023-07-30 22:45:47 -0400

    Added start page. Included timer that starts when 'start' button is clicked.

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,63 @@
 
+let start = document.querySelector("#start");
+
 let h1El = document.querySelector("#question");
+let h4El = document.querySelector("h4")
 
 let quizDiv = document.querySelector("#quiz");
 
-
 let time = 90;
+let score = 0;
 
 let questionButton1 = document.querySelector("#answer1");
 let questionButton2 = document.querySelector("#answer2");
 let questionButton3 = document.querySelector("#answer3");
 let questionButton4 = document.querySelector("#answer4");
 
+let answer1 = "Great";
+
 let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-let timerID = setInterval(function(){
 
-    time = time-=1;
 
-}, 1000)
 
-//call clearInterval(timerID) when quiz is over
-
-let questions = [{question: "How are you?", answers: ["Great", "OK", "Not great", "Terrible"], correctAnswer: "Great" },
-                {question: "Who were you yesterday?", answers: ["Great", "OK", "Not great", "Terrible"], correctAnswer: "Great" }
+let questions = [{question: "How are you?", 
+                answers: ["Great", "OK", "Not great", "Terrible"], correctAnswer: "Great" },
+                {question: "Who were you yesterday?", 
+                answers: ["Great", "OK", "Not great", "Terrible"], correctAnswer: "Great" },
+                {question: "How are you?", 
+                answers: ["Great", "OK", "Not great", "Terrible"], correctAnswer: "Great" },
+                {question: "Who are you?", 
+                answers: ["Great", "OK", "Not great", "Terrible"], correctAnswer: "Great" },
+                {question: "Where are you?", 
+                answers: ["Great", "OK", "Not great", "Terrible"], correctAnswer: "Great" }
 ]
+
 
 let currentQuestion = 0;
 
-renderQuestion();
+
+start.addEventListener("click", function() {
+    
+    let countdown = setInterval(function(){
+
+        time = time-=1;
+        h4El.textContent = "Time Remaining: " + time;
+    
+        if (time === 0) {
+            clearInterval(countdown);
+        }
+    
+    }, 1000)
+    
+    start.style.display = 'none';
+    renderQuestion();
+    quizDiv.style.display = 'flex';
+    
+
+    
+})
+
 
 
 function renderQuestion() {
@@ -44,11 +74,19 @@ questionButton4.textContent = questions[currentQuestion].answers[3]
 
 
 quizDiv.addEventListener("click", function(event){
+
+
+    if (answer1 === questions[currentQuestion].correctAnswer) {
+       console.log(score++);
+    }  else {
+        time -= 10;
+    }
+
     if(event.target.matches("button")){
-        console.log("click!");
         currentQuestion++
         renderQuestion();
-    }
+    } 
+    
 })
 
 //put JSON.stringify in the button that saves the score and player initials
