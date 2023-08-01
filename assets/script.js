54307b4b38b80d47b746e2d4aff751b54a7b317e
pb1983 <phil.bryer@gmail.com>
2023-07-31 23:15:08 -0400

    Created highScores.js file. Added styling to css for highscores.html. Added event listener to finalScore section. Included additional conditionals to timer.

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,25 @@
 
 let start = document.querySelector("#start");
-
 let h1El = document.querySelector("#question");
 let h4El = document.querySelector("h4")
-
+let finalMessage = document.querySelector("#finalMessage");
+let correctAnswer = document.querySelector('#correct');
+let wrongAnswer = document.querySelector('#incorrect');
 let quizDiv = document.querySelector("#quiz");
-
-let time = 90;
+let time = 60;
 let score = 0;
+let currentQuestion = 0;
+let initScore = document.querySelector("#initScore");
+
+
+
+// let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
 
 let questionButton1 = document.querySelector("#answer1");
 let questionButton2 = document.querySelector("#answer2");
 let questionButton3 = document.querySelector("#answer3");
 let questionButton4 = document.querySelector("#answer4");
-
-let finalScore = document.querySelector("#finalScore");
-
-let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-let answer1 = "Great";
 
 
 let questions = [{question: "How are you?", 
@@ -34,9 +35,6 @@ let questions = [{question: "How are you?",
 ]
 
 
-let currentQuestion = 0;
-
-
 start.addEventListener("click", function() {
     
     let countdown = setInterval(function(){
@@ -46,15 +44,16 @@ start.addEventListener("click", function() {
     
         if (time === 0) {
             clearInterval(countdown);
-        }
+            quizDiv.style.display = 'none';
+            finalScore.style.display = 'flex';
+            finalMessage.textContent = "Time is up! Your score is " + score + ". Please enter your initials below";
+        } 
     
     }, 1000)
     
     start.style.display = 'none';
     renderQuestion();
     quizDiv.style.display = 'flex';
-    
-
     
 })
 
@@ -70,37 +69,51 @@ questionButton3.textContent = questions[currentQuestion].answers[2]
 questionButton4.textContent = questions[currentQuestion].answers[3]
 
 
+
 }
 
 
 quizDiv.addEventListener("click", function(event){
 
-
-console.log(event.target);
-
     if(event.target.matches("button")) {
 
         if(event.target.textContent === questions[currentQuestion].correctAnswer) {
-            console.log("User chose correct answer");
+            correctAnswer.textContent = "Correct!";
+            score += 1;
         } else {
-            console.log("Wrong Answer")           
+            wrongAnswer.textContent = "Incorrect. 10 seconds will be deducted from the clock.";         
             time -= 10;
         }
 
-        
+
         currentQuestion++
-        console.log("on question: ", currentQuestion, "is quiz over: ", currentQuestion >= questions.length-1)
+
+        
         if (currentQuestion >= questions.length-1) {
             quizDiv.style.display = 'none';
             finalScore.style.display = 'flex';
         }
-    renderQuestion();
-    } 
 
+    renderQuestion();
     
-    
+    } 
+    quizEnd();
+})
+
+    function quizEnd() {    
+
+     
+        finalMessage.textContent = "All done! Your score is " + score + ". Please enter your initials below";
+
+        localStorage.setItem("score", score);
+    }
+
+   
+finalScore.addEventListener("click", function(event) {
+    let initials = document.querySelector("#initBox").value;
+    localStorage.setItem("initials", initials);
 })
 
 
 
-//put JSON.stringify in the button that saves the score and player initials
+//localStorage.setItem("highScores", JSON.stringify([{"initials": "dre", "score": 80}]))
